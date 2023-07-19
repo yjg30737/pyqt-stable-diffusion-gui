3c72f371d150f47a5294220c3b387a5a7f4a852f
Jung Gyu Yoon <yjg30737@gmail.com>
2023-07-19 15:32:29 +0900

    Bump ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='pyqt-stable-diffusion-gui',
-    version='0.0.13',
+    version='0.0.131',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
